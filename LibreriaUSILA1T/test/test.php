0f1df8bd8dbd5a77d00050f9cfc03b6d9b2ee92c
unknown <alumno@.(none)>
2011-10-12 17:12:28 -0500

    arreglo de objetos

--- LibreriaUSILA1T/test/test.php
+++ LibreriaUSILA1T/test/test.php
@@ -1,7 +1,21 @@
 <?php
 require_once '/../ds/SQL.php';
 require_once '/../dm/MySQL.php';
+require_once '/../model/Libro.php';
+require_once '/../persistence/Persistence.php';
+require_once '/../ds/BaseDeDatos.php';
 
+
+
+
+$miLibro = new Libro();
+
+print_r($miLibro->listar());
+/*
+$consulta = new SQL();
+$consulta->addTable('libros');
+echo Persistence::consultar($consulta);
+*//*
 $consulta = new SQL();
 $consulta->addTable('libros');
 
@@ -11,7 +25,7 @@ $resultado = $mySql->traerDatos($consulta);
 $mySql->desconectar();
 print_r($resultado);
 
-
+*/
 //$mySql->addWhere('id=1');
 
 //echo "SQL:".$mySql;
